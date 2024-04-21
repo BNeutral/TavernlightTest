32e9448b0337b9787ff029f2cdc6121516433323
BNeutral <BNeutral@users.noreply.github.com>
2024-04-21 08:16:09 -0300

    Answered question 5. The area size is slightly wrong in my client for some reason

---
+++ Questions 3-7/tfs-v1.4-windows-msvc-Release-luajit/data/spells/scripts/support/frigo.lua
@@ -0,0 +1,40 @@
+-- Probably not the best implementation, but the documentation is lacking
+-- useful reference https://tibia.fandom.com/wiki/Effects
+
+local duration = 4000
+local interval = 500
+local fullArea = AREA_FRIGO_BASE
+local combats = {}
+
+local function randomizeArea()
+    local randomizedArea = {}
+    for i = 1, #fullArea do
+        randomizedArea[i] = {}
+        for j = 1, #fullArea[i] do
+            if fullArea[i][j] == 1 then
+                randomizedArea[i][j] = math.random(0, 1) -- Make it 0 or 1 randomly
+            else
+                randomizedArea[i][j] = fullArea[i][j]
+            end
+        end
+    end
+    return randomizedArea
+end
+
+for _=0, duration, interval do
+	local combat = Combat()
+	combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+	local area = createCombatArea(randomizeArea())
+	combat:setArea(area)
+	table.insert(combats, combat)
+end
+
+function onCastSpell(creature, variant)
+    for i, combat in ipairs(combats) do
+		local function exec()
+			return combat:execute(creature, variant)
+		end
+		addEvent(exec, interval*i - interval) -- lua arrays start from 1, but we want the first one to start at 0
+	end
+	return nil -- is this fine?
+end
